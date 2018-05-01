d6502b57e74237d0ca99d566b460e0e43590cb95
treeword <38619149+treeword@users.noreply.github.com>
2018-05-01 21:40:52 +0800

    Delete 1520934 汪顺 汉诺塔.py

--- 1520934 汪顺 汉诺塔.py
+++
@@ -1,7 +0,0 @@
-def move(n,a,b,c):
-    if n==1:
-        print('将%s上的%s从%s->%s'%(a,n,a,c))
-    else:
-        move(n-1,a,c,b)     
-        print('将%s上的%s从%s->%s'%(a,n,a,c)) 
-        move(n-1,b,a,c) 
