05951f8691d666ec7a15876b75ba115881219a04
treeword <38619149+treeword@users.noreply.github.com>
2018-05-09 00:02:00 +0800

    Add files via upload

---
+++ 1520934汪顺/char.py
@@ -0,0 +1,33 @@
+# -*- coding:utf8 -*-
+import string
+from collections import namedtuple
+
+def str_count(s):
+    '''找出字符串中的中英文、空格、数字、标点符号个数'''
+    count_en = count_dg = count_sp = count_zh = count_pu = 0
+
+    s_len = len(s)
+    for c in s:
+        # 英文
+        if c in string.ascii_letters:
+            count_en += 1
+        # 数字
+        elif c.isdigit():
+            count_dg += 1
+        # 空格
+        elif c.isspace():
+            count_sp += 1
+        # 中文
+        elif c.isalpha():
+            count_zh += 1
+        # 特殊字符
+        else:
+            count_pu += 1
+
+    total_chars = count_zh + count_en + count_sp + count_dg + count_pu
+    if total_chars == s_len:
+        return namedtuple('Count', ['total', 'zh', 'en', 'space', 'digit', 'punc'])(s_len, count_zh, count_en,count_sp, count_dg, count_pu)
+    else:
+        print('Something is wrong!')
+        return None
+
